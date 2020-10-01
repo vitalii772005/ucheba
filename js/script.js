4ebac4a0c7517c7ca7b0087140de154a21fa1316
Vitaliy <example@com.ru>
2020-10-01 22:06:08 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use sctrict";
+//const result = confirm('Вам есть 18 лет?');
+//console.log(result);
+/** 
+const answer = [];
+answer[0]= prompt('Как ваше имя?','');
+answer[1]= prompt('Сколько вам лет?','');
+document.write(answer); **/
+const interp = 'toys';
+console.log(`https://mysite.com/${interp}/5`);
+const user = 'ivan';
+alert(`привет, ${user}`);
