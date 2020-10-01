0540e146e65522b7869c4c89ee13f8effa92127c
Vitaliy <example@com.ru>
2020-10-01 23:21:18 +0300

    crate first exesaise

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-"use sctrict";
-//const result = confirm('Вам есть 18 лет?');
-//console.log(result);
-/** 
-const answer = [];
-answer[0]= prompt('Как ваше имя?','');
-answer[1]= prompt('Сколько вам лет?','');
-document.write(answer); **/
-const interp = 'toys';
-console.log(`https://mysite.com/${interp}/5`);
-const user = 'ivan';
-alert(`привет, ${user}`);
+
+
+const numberOfilms = +prompt('Сколько фильмов вы уже посмотрели','');
+//создаем обект
+const personalObjert ={
+
+    count: numberOfilms,
+    movies: {},
+    actors: {},
+    genres:[],
+    privat:false
+};
+
+const a = prompt('Один из просмотренных последних фильмов?',''),
+      B = prompt('На сколько оцените его',''),
+      c = prompt('Один из просмотренных последних фильмов?',''),
+      d = prompt('На сколько оцените его','');
+
+      personalObjert.movies[a] = B;
+      personalObjert.movies[c] = d;
+      console.log(personalObjert);
+
