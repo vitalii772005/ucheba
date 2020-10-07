ff435d34340047eff91348317e9640a903f985e8
Vitaliy <example@com.ru>
2020-10-07 11:00:49 +0300

    'practik2'

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-
-
+'use strict' 
+/**  */
 const numberOfilms = +prompt('Сколько фильмов вы уже посмотрели','');
 //создаем обект
 const personalObjert ={
@@ -11,12 +11,31 @@ const personalObjert ={
     privat:false
 };
 
-const a = prompt('Один из просмотренных последних фильмов?',''),
-      B = prompt('На сколько оцените его',''),
-      c = prompt('Один из просмотренных последних фильмов?',''),
-      d = prompt('На сколько оцените его','');
+//const a = prompt('Один из просмотренных последних фильмов?',''),
+//      B = prompt('На сколько оцените его',''),
+//      c = prompt('Один из просмотренных последних фильмов?',''),
+//      d = prompt('На сколько оцените его','');
 
-      personalObjert.movies[a] = B;
-      personalObjert.movies[c] = d;
-      console.log(personalObjert);
+     // personalObjert.movies[a] = B;
+     // personalObjert.movies[c] = d;
+
+      for (let i=0; i<2; i++) {
+        const a = prompt('Один из просмотренных последних фильмов?',''),
+              b = prompt('На сколько оцените его','');
+               //пустые ковычки - пустое поле (нажал ок не вводя ничего), a.length - длина 
+              //null пользователь нажал отмену
+              if (a != null && b != null && a != "" && b !="" && a.length < 50)  {
+                personalObjert.movies[a] = b;
+                console.log(personalObjert);
+               }  else  {
+                 console.log('error');     
+                 i--;
+                }
+              
+          if ()
+     }
+
+
+//const num = 6;
+//(num===6) ? console.log ('ok') : console.log('error');
 
